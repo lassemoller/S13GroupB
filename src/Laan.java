5aa007ba4a8c1ded9a00e43c8f53186b6e3bfcab
lassemoller <lassemoller27@gmail.com>
2013-02-23 17:44:23 +0100

    laan class started

---
+++ src/Laan.java
@@ -0,0 +1,45 @@
+public class Laan {
+
+	Person laaner;
+	//BogEksemplar bog;
+	//NewGregorianCalender datoUd;
+	//NewGregorianCalender datoHjem;
+	double boede;
+	
+ public Person laaner(){
+ return this.laaner;
+ }
+ 
+// public Date retuneresDato(){
+
+ //}
+
+ public void retunerer(){
+
+ }
+ 
+ public void retunerer(int yr, int mt, int dy){
+	 
+ }
+ 
+//string output format
+	public String toString() {
+		String s = String.format("formating testing output");
+		return s;
+	}
+	
+	private int antalDage(){
+		return 0;	
+	}
+	
+	private double beregnBoede(){
+		return boede;
+	}
+	
+	
+	public static void main(String[] args) {
+
+
+	}
+ 
+ }
